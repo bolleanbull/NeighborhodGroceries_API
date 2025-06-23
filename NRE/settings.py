eac4ad0cc0c532e32296b956da9f31b64f1311f2
unknown <bolleanbull@gmail.com>
2025-06-22 20:55:46 -0500

    env modfication

--- NRE/settings.py
+++ NRE/settings.py
@@ -3,13 +3,15 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+import os
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l#pffp5_jkd29pazizud=z#uk%9upiku3sy-sop3q-x_*ov&#_'
+
+SECRET_KEY = os.getenv('SECRET_KEY',None)
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
